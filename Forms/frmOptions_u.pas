f26861fb827c0798cb3fc1879b5bbf6ba5a57d0d
Bob Mannino <manninobobby@icloud.com>
2025-05-16 21:43:51 +0100

    add empty second form

---
+++ Forms/frmOptions_u.pas
@@ -0,0 +1,24 @@
+unit frmOptions_u;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+
+type
+  TfrmOptions = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmOptions: TfrmOptions;
+
+implementation
+
+{$R *.fmx}
+
+end.
