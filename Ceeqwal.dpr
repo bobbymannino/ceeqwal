2b4c268c411631c69cb779b87f8bb9a8e3fd461a
Bob Mannino <manninobobby@icloud.com>
2025-05-16 21:44:09 +0100

    add more inputs for connection

--- Ceeqwal.dpr
+++ Ceeqwal.dpr
@@ -3,12 +3,14 @@ program Ceeqwal;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  frmMain_u in 'Forms\frmMain_u.pas' {frmMain};
+  frmMain_u in 'Forms\frmMain_u.pas' {frmMain},
+  frmOptions_u in 'Forms\frmOptions_u.pas' {frmOptions};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TfrmMain, frmMain);
+  Application.CreateForm(TfrmOptions, frmOptions);
   Application.Run;
 end.
--- Forms/frmMain_u.pas
+++ Forms/frmMain_u.pas
@@ -6,17 +6,22 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes,
   System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
-  FMX.Controls.Presentation, FMX.Edit;
+  FMX.Controls.Presentation, FMX.Edit, frmOptions_u;
 
 type
   TfrmMain = class(TForm)
-    inpConnectionString: TEdit;
+    inpServer: TEdit;
     btnTestConnection: TButton;
     btnConnect: TButton;
     btnClose: TButton;
+    inpUser: TEdit;
+    inpPwd: TEdit;
+    lblServer: TLabel;
+    lblUser: TLabel;
+    lblPwd: TLabel;
     procedure btnTestConnectionClick(Sender: TObject);
-    procedure btnConnectClick(Sender: TObject);
     procedure btnCloseClick(Sender: TObject);
+    procedure btnConnectClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -36,27 +41,18 @@ begin
 end;
 
 procedure TfrmMain.btnConnectClick(Sender: TObject);
+var
+  frm: TForm;
 begin
-  //
+  frm := TfrmOptions.Create(nil);
+  Application.MainForm := frm;
+  close;
+  frm.Show;
 end;
 
 procedure TfrmMain.btnTestConnectionClick(Sender: TObject);
-var
-  connStr: string;
-  connValid: boolean;
 begin
-  connStr := inpConnectionString.Text.Trim;
-
-  connValid := connStr <> EmptyStr;
-  if not connValid then
-  begin
-    ShowMessage('Connection string is empty');
-    Exit;
-  end;
-
-  { TODO : Test string valid via regex first }
-
-  ShowMessage('Connection string valid');
+  //
 end;
 
 end.
