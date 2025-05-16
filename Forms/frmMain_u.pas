330fa11399139ddf3bd2e5145e5280349ed2324b
Bob Mannino <manninobobby@icloud.com>
2025-05-16 21:51:28 +0100

    add testing of connection
    also add database with default 'master'

--- Forms/frmMain_u.pas
+++ Forms/frmMain_u.pas
@@ -6,7 +6,11 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes,
   System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
-  FMX.Controls.Presentation, FMX.Edit, frmOptions_u;
+  FMX.Controls.Presentation, FMX.Edit,
+  FireDAC.Phys.MSSQL, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Comp.Client,
+  frmOptions_u;
 
 type
   TfrmMain = class(TForm)
@@ -19,6 +23,8 @@ type
     lblServer: TLabel;
     lblUser: TLabel;
     lblPwd: TLabel;
+    inpDatabase: TEdit;
+    lblDatabase: TLabel;
     procedure btnTestConnectionClick(Sender: TObject);
     procedure btnCloseClick(Sender: TObject);
     procedure btnConnectClick(Sender: TObject);
@@ -51,8 +57,30 @@ begin
 end;
 
 procedure TfrmMain.btnTestConnectionClick(Sender: TObject);
+var
+  Connection: TFDConnection;
 begin
-  //
+  Connection := TFDConnection.Create(nil);
+  try
+    Connection.Params.Clear;
+    Connection.Params.DriverID := 'MSSQL';
+    Connection.Params.Values['Server'] := inpServer.Text;
+    Connection.Params.Values['Database'] := inpDatabase.Text;
+    Connection.Params.Values['User_Name'] := inpUser.Text;
+    Connection.Params.Values['Password'] := inpPwd.Text;
+
+    try
+      Connection.Connected := True;
+      ShowMessage('1'); // Success
+    except
+      on E: Exception do
+      begin
+        ShowMessage('0'); // Failure
+      end;
+    end;
+  finally
+    Connection.Free;
+  end;
 end;
 
 end.
