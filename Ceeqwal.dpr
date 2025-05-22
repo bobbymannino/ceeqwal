a51fbcf39879abe34238ec4d82328471391d8b11
Bob Mannino <manninobobby@icloud.com>
2025-05-22 15:26:10 +0100

    feat/connect screen (#5)
    * get ui working for main form (connection window)
    
    - test connection button
    - connect button
    - close button
    
    * add empty second form
    
    * add more inputs for connection
    
    * fix tab order on connect
    
    * add testing of connection
    
    also add database with default 'master'
    
    * default user 'sa'
    
    * added default server on debug
    
    * factor out connect logic and show second screen if valid
    
    resolves #1
    
    * work on uploading credentials
    
    user can upload file (but nothing happens post)
    
    * feat: upload txt file for credentials
    
    resloves #2

--- Ceeqwal.dpr
+++ Ceeqwal.dpr
@@ -3,12 +3,14 @@ program Ceeqwal;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  frmMain_u in 'Forms\frmMain_u.pas' {Form1};
+  frmMain_u in 'Forms\frmMain_u.pas' {frmMain},
+  frmOptions_u in 'Forms\frmOptions_u.pas' {frmOptions};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.CreateForm(TfrmOptions, frmOptions);
   Application.Run;
 end.
--- Forms/frmMain_u.pas
+++ Forms/frmMain_u.pas
@@ -3,22 +3,171 @@ unit frmMain_u;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants, System.RegularExpressions,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Controls.Presentation, FMX.Edit,
+  FireDAC.Phys.MSSQL, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Comp.Client,
+  frmOptions_u;
 
 type
-  TForm1 = class(TForm)
+  TfrmMain = class(TForm)
+    inpServer: TEdit;
+    btnTestConnection: TButton;
+    btnConnect: TButton;
+    btnClose: TButton;
+    inpUser: TEdit;
+    inpPwd: TEdit;
+    lblServer: TLabel;
+    lblUser: TLabel;
+    lblPwd: TLabel;
+    inpDatabase: TEdit;
+    lblDatabase: TLabel;
+    btnUpload: TButton;
+    fileInpCredentials: TOpenDialog;
+    procedure btnTestConnectionClick(Sender: TObject);
+    procedure btnCloseClick(Sender: TObject);
+    procedure btnConnectClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure btnUploadClick(Sender: TObject);
   private
-    { Private declarations }
+    function CheckConnection: boolean;
+    function ParseCredentials(const aField, aTxt: string): string;
   public
     { Public declarations }
   end;
 
 var
-  Form1: TForm1;
+  frmMain: TfrmMain;
 
 implementation
 
 {$R *.fmx}
+
+procedure TfrmMain.btnCloseClick(Sender: TObject);
+begin
+  Application.Terminate;
+end;
+
+procedure TfrmMain.btnConnectClick(Sender: TObject);
+var
+  frm: TForm;
+begin
+  if not CheckConnection then
+  begin
+    ShowMessage('Failed to connect');
+
+    Exit;
+  end;
+
+  frm := TfrmOptions.Create(nil);
+  Application.MainForm := frm;
+  close;
+  frm.Show;
+end;
+
+procedure TfrmMain.btnTestConnectionClick(Sender: TObject);
+var
+  conn: TFDConnection;
+begin
+  if CheckConnection then
+    ShowMessage('Succeeded')
+  else
+    ShowMessage('Failed to connect');
+end;
+
+procedure TfrmMain.btnUploadClick(Sender: TObject);
+var
+  fileContent: TStringList;
+  val : string;
+begin
+  fileInpCredentials.InitialDir := GetCurrentDir;
+
+  if not fileInpCredentials.Execute then
+    Exit;
+
+  fileContent := TStringList.Create;
+  try
+    try
+      fileContent.LoadFromFile(fileInpCredentials.FileName);
+
+      val := ParseCredentials('Server', fileContent.Text);
+      if val <> EmptyStr then
+        inpServer.Text := val;
+
+      val := ParseCredentials('Database', fileContent.Text);
+      if val <> EmptyStr then
+        inpDatabase.Text := val;
+
+      val := ParseCredentials('User', fileContent.Text);
+      if val <> EmptyStr then
+        inpUser.Text := val;
+
+      val := ParseCredentials('Password', fileContent.Text);
+      if val <> EmptyStr then
+        inpPwd.Text := val;
+
+    except
+      on e: Exception do
+      begin
+        ShowMessage('Failed');
+      end;
+    end;
+  finally
+    fileContent.Free;
+  end;
+end;
+
+function TfrmMain.CheckConnection: boolean;
+var
+  conn: TFDConnection;
+begin
+  Result := False;
+
+  conn := TFDConnection.Create(nil);
+  try
+    conn.Params.Clear;
+    conn.Params.DriverID := 'MSSQL';
+    conn.Params.Values['Server'] := inpServer.Text;
+    conn.Params.Values['Database'] := inpDatabase.Text;
+    conn.Params.Values['User_Name'] := inpUser.Text;
+    conn.Params.Values['Password'] := inpPwd.Text;
+
+{$IFDEF DEBUG}
+    { TODO : Move this to checkbox on form }
+    conn.Params.Values['OSAuthent'] := 'Yes';
+{$ENDIF}
+    conn.Connected := True;
+
+  finally
+    conn.Free;
+
+    Result := True;
+  end;
+end;
+
+procedure TfrmMain.FormCreate(Sender: TObject);
+begin
+{$IFDEF DEBUG}
+  inpServer.Text := 'DESKTOP-5EFKG00\SQLEXPRESS';
+{$ENDIF}
+end;
+
+function TfrmMain.ParseCredentials(const aField, aTxt: string): string;
+var
+  ptn: string;
+  mtch: TMatch;
+begin
+  Result := '';
+
+  ptn := Format('^%s=(\w+)\r?\n?$', [aField]);
+
+  mtch := TRegEx.Match(aTxt, ptn, [TRegExOption.roMultiLine]);
+
+  if mtch.Success then
+    Result := mtch.Groups[1].Value;
+end;
 
 end.
