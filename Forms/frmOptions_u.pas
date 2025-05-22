71db615ab32da322648a086ceeece52587689d53
Bob Mannino <manninobobby@icloud.com>
2025-05-22 15:51:45 +0100

    ui ready for running queries

--- Forms/frmOptions_u.pas
+++ Forms/frmOptions_u.pas
@@ -3,11 +3,19 @@ unit frmOptions_u;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Memo.Types,
+  FMX.StdCtrls, FMX.Controls.Presentation, FMX.ScrollBox, FMX.Memo;
 
 type
   TfrmOptions = class(TForm)
+    inpQry: TMemo;
+    btnRunQry: TButton;
+    btnClose: TButton;
+    procedure btnCloseClick(Sender: TObject);
+    procedure btnRunQryClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +28,23 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TfrmOptions.btnCloseClick(Sender: TObject);
+begin
+  Application.Terminate;
+end;
+
+procedure TfrmOptions.btnRunQryClick(Sender: TObject);
+begin
+  //
+end;
+
+procedure TfrmOptions.FormCreate(Sender: TObject);
+begin
+{$IFDEF DEBUG}
+  inpQry.Lines.Add('select *');
+  inpQry.Lines.Add('from Customer;');
+{$ENDIF}
+end;
 
 end.
