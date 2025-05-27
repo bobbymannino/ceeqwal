f5eca5210db712363ff166d653c8b1cbfb4a543a
Bob Mannino <manninobobby@icloud.com>
2025-05-27 21:13:38 +0100

    feat: show results on select qry

--- Forms/frmOptions_u.pas
+++ Forms/frmOptions_u.pas
@@ -6,13 +6,19 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes,
   System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Memo.Types,
-  FMX.StdCtrls, FMX.Controls.Presentation, FMX.ScrollBox, FMX.Memo;
+  FMX.StdCtrls, FMX.Controls.Presentation, FMX.ScrollBox, FMX.Memo,
+  Data.Bind.EngExt, FMX.Bind.DBEngExt, System.Rtti,
+  System.Bindings.Outputs, FMX.Bind.Editors, Data.Bind.Components,
+  dmDb_u, FMX.Grid.Style, FMX.Grid;
 
 type
   TfrmOptions = class(TForm)
     inpQry: TMemo;
     btnRunQry: TButton;
     btnClose: TButton;
+    BindingsList1: TBindingsList;
+    LinkControlToPropertySQLText: TLinkControlToProperty;
+    grd: TGrid;
     procedure btnCloseClick(Sender: TObject);
     procedure btnRunQryClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
@@ -35,8 +41,29 @@ begin
 end;
 
 procedure TfrmOptions.btnRunQryClick(Sender: TObject);
+var
+  col: TStringColumn;
 begin
-  //
+  with dmDb do
+  begin
+    qry.SQL.Text := inpQry.Lines.Text;
+
+    try
+      qry.Open;
+
+      grd.ClearColumns;
+
+      for var i := 0 to qry.FieldCount - 1 do
+      begin
+        col := TStringColumn.Create(grd);
+        col.Header := qry.Fields[i].FieldName;
+        col.Width := Length(qry.Fields[i].FieldName) + 16;
+        grd.AddObject(col);
+      end;
+    finally
+      qry.Close;
+    end;
+  end;
 end;
 
 procedure TfrmOptions.FormCreate(Sender: TObject);
